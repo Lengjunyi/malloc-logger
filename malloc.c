0048c20ed4a5f5ada4184b926f0839599cfe14a5
Alan (Jiacheng) Liang <alan.liang20@imperial.ac.uk>
2022-01-30 21:28:42 +0000

    formatted output to stderr

--- malloc.c
+++ malloc.c
@@ -10,18 +10,20 @@
 void *malloc(size_t size) {
     static void *(*real_malloc)(size_t);
     static bool loading = false;
-    fprintf(stderr, "%ld %d\n", size, loading);
+    static size_t total_allocation = 0;
 
     if (loading) {
-        return mmap(NULL, size, PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);;
+        return mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, 0, 0);
     }
     else if (!real_malloc) {
         loading = true;
         real_malloc = (void * (*)(size_t))dlsym(RTLD_NEXT, "malloc");
-        fprintf(stderr, "returning from dlsym\n");
         loading = false;
     }
     
+    total_allocation += size;
+    fprintf(stderr, "Total bytes allocated so far: %ld\n", total_allocation);
+
     return (*real_malloc)(size);
 }
 
